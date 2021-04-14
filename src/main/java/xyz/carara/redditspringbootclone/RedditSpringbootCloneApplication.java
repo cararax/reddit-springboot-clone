fc585af1a51add2b4f16647faf51b98d3247ce55
Pedro Carara <cararax@gmail.com>
2021-04-14 10:01:39 -0300

    startei o spring

---
+++ src/main/java/xyz/carara/redditspringbootclone/RedditSpringbootCloneApplication.java
@@ -0,0 +1,13 @@
+package xyz.carara.redditspringbootclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditSpringbootCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditSpringbootCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/carara/redditspringbootclone/RedditSpringbootCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.carara.redditspringbootclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditSpringbootCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
